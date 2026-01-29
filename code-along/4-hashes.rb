f42114949da4db232213cb06f7cdbdbaba9e7056
WilliamHsiang <william.hsiang@kellogg.northwestern.edu>
2026-01-29 02:31:57 +0000

    done 4-hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,35 @@
 
 # Hashes are lists of *key-value pairs*
 
+profile = {
+  "name" => "William",
+  "location" => "Evanston",
+  "status" => "unemployed"
+}
+
+
 # Accessing data from the hash
 
+name = profile["name"]
+puts "Hello #{name}"
+
+profile["age"] = 31
+profile["name"] = "William Hsiang"
+p profile
+
 # More Complex Hashes
+
+profile["location"] = {"city" => "Evanston", "state" => "IL"}
+p profile
+puts profile["location"]["city"]
+
+timeline = [
+  {"status" => "eating", "datetime" => "10AM"},
+  {"status" => "playing", "datetime" => "11AM"},
+  {"status" => "sleeping", "datetime" => "12PM"}
+]
+
+profile["timeline"] = timeline
+
+p profile
+puts profile["timeline"][0]["status"]
