625bbee9deab28f16f76a95eab258d53f64b5979
WilliamHsiang <william.hsiang@kellogg.northwestern.edu>
2026-01-29 01:23:18 +0000

    done 2-conditionals

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,52 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+puts 3 > 2
+puts 2 > 3
+puts 3 == 2
+puts 3 != 2
 
 # If Conditional Logic
+if 3 > 2
+  puts "blackpink in your area"
+end
 
 # If/Else Conditional Logic
 
+if false
+  puts "blackpink in your area"
+else
+  puts "blackpink in my area"
+end
+
 # Elsif Conditional Logic
 
+jennie = 1
+jisoo = 2
+
+if jennie > 1
+  puts "blackpink in your area"
+elsif jennie = jisoo
+  puts "blackpink in my area"
+else
+  puts "blackpink everywhere"
+end
+
 # Combining Expressions
+
+a = 1
+b = 2
+
+# and
+if a == 1 && b == 2
+  puts true
+end
+
+# or
+if a == 1 || b == 1
+  puts true
+end
