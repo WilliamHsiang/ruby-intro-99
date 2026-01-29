69709f1187bcabf9c3fe2ee9fdbe9fe1f4ccc7b0
WilliamHsiang <william.hsiang@kellogg.northwestern.edu>
2026-01-29 02:14:43 +0000

    done 3-arrays

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,20 @@
 
 # Arrays are lists of things
 
+blackpink = ["jennie", "lisa", "rose", "jisoo"]
+p blackpink
+
 # Accessing the array
+puts blackpink[0]
+p blackpink[-1]
 
 # Add to the array
+
+yg = blackpink + ["babymonster"]
+p yg
+
+yg.push "bigbang"
+p yg
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
